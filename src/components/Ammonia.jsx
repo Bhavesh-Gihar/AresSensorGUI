3f26211401b12a7455839ddd0f5267f2b66dc2a1
Bhavesh-Gihar <97470501+Bhavesh-Gihar@users.noreply.github.com>
2023-02-21 10:03:46 -0500

    Update Ammonia.jsx

--- src/components/Ammonia.jsx
+++ src/components/Ammonia.jsx
@@ -2,10 +2,12 @@ import React, { Component } from "react";
 import { GiChemicalDrop } from "react-icons/gi";
 import '../App.css';
 import { Line } from "react-chartjs-2";
+import 'chartjs-plugin-streaming';
+import moment from "moment";
 
 class Ammonia extends Component {
     state = {
-      value: 0,
+      value: 0.3,
       ros: null
     };
   
@@ -63,6 +65,76 @@ class Ammonia extends Component {
       top: "0.6rem",
       left: "2.5rem"
     }
+
+    dataGraph = {
+      datasets: [
+        {
+          label: "Dataset 1 (linear interpolation)",
+          backgroundColor: '#CF0A0A',
+          borderColor: '#DC5F00',
+          fill: false,
+          lineTension: 0,
+          borderDash: [8, 4],
+          data: []
+        }
+      ]
+    };
+
+
+  options = {
+    elements: {
+      line: {
+        tension: 0.5
+      }
+    },
+    scales: {
+      xAxes: [
+        {
+          type: "realtime",
+          distribution: "linear",
+          realtime: {
+            onRefresh: (chart) => {
+              console.log(this);
+              var x = moment()
+              var y = Math.random();
+              chart.data.datasets[0].data.push({
+                x: x,
+                y: y
+              });
+              this.setState({
+                value: y
+              });
+            },
+            delay: 3000,
+            time: {
+              displayFormat: "h:mm"
+            }
+          },
+          ticks: {
+            displayFormats: 1,
+            maxRotation: 0,
+            minRotation: 0,
+            stepSize: 1,
+            maxTicksLimit: 30,
+            minUnit: "second",
+            source: "auto",
+            autoSkip: true,
+            callback: function(value) {
+              return moment(value, "HH:mm:ss").format("mm:ss");
+            }
+          }
+        }
+      ],
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+            max: 1
+          }
+        }
+      ]
+    }
+  };
   
     render() {
       return ( 
@@ -74,25 +146,12 @@ class Ammonia extends Component {
             <div><GiChemicalDrop style={this.styles}/></div>
             <div className="dataValue">
               <h6>Methane reading: </h6>
-              <h4>{this.state.value} PPM</h4>
+              <h4>{this.state.value.toFixed(2)} PPM</h4>
             </div>
           </div>
           <div className="graph">
-          <Line
-            data={this.state.value}
-            options={{
-              plugins: {
-              title: {
-              display: true,
-              text: "Users Gained between 2016-2020"
-            },
-            legend: {
-              display: false
-            }
-          }
-        }}
-        />
-        </div>
+            <Line data={this.dataGraph} options={this.options} />
+          </div>
       </div>
       );
     }
